c6add9f57b8cc91ba6ad4057af10bccab544e0c3
J. Santos <jamillo@gmal.com>
2012-08-18 21:03:59 -0300

    Início do desenvolvimento do cenário

---
+++ classes/maps/Map.rb
@@ -0,0 +1,22 @@
+
+class Map
+
+	def initialize()
+		@area = CP::Vec2.new(1000, 480)
+	end
+
+	protected
+		#
+
+	public
+		def area
+			@area
+		end
+
+		def area=(value)
+			@area = value
+		end
+
+		
+end
+
